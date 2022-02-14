835671d2cea57325742b30d1219685ef86b7c5b4
Ludovic Delauné <message@cartodev.com>
2022-02-14 11:46:03 +0100

    remove trombi and unused code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -31,42 +31,5 @@ export default class Home extends Vue {
   get posts(): Post[] {
     return this.$store.state.posts;
   }
-
-  isSignedUp = false;
-
-  form = {
-    email: '',
-  };
-
-  encode(data: Record<string, unknown>): string {
-    return Object.keys(data)
-      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-      .join('&');
-  }
-
-  validEmail(email: string): boolean {
-    // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-
-  async handleSubmit(): Promise<void> {
-    if (!this.validEmail(this.form.email)) {
-      this.$refs.emailInput.focus();
-      return;
-    }
-
-    try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: this.encode({ 'form-name': 'signups', ...this.form }),
-      });
-
-      this.isSignedUp = true;
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
 </script>
